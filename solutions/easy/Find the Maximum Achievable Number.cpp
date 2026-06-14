be747ca661046cd838758747e868f982652f083a
Bhushan Vidhate <bhushanvidhate109@gmail.com>
2026-06-15 00:33:23 +0530

    Add solution for Find the Maximum Achievable Number

---
+++ solutions/easy/Find the Maximum Achievable Number.cpp
@@ -0,0 +1,12 @@
+// Title: Find the Maximum Achievable Number
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/find-the-maximum-achievable-number/
+
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) {
+        return num + (t*2);
+    }
+        
+};
