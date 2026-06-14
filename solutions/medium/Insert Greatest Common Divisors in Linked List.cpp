7d4d0b8f9c2836a887dfdb45ee376837b48942c9
Bhushan Vidhate <bhushanvidhate109@gmail.com>
2026-06-15 00:28:02 +0530

    Add solution for Insert Greatest Common Divisors in Linked List

---
+++ solutions/medium/Insert Greatest Common Divisors in Linked List.cpp
@@ -0,0 +1,16 @@
+// Title: Insert Greatest Common Divisors in Linked List
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/insert-greatest-common-divisors-in-linked-list/
+
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertGreatestCommonDivisors(ListNode* head) {
