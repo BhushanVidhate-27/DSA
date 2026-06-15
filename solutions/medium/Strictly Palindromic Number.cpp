8e06c1ff05789ff90bbfe0acf4681701b9efce05
Bhushan Vidhate <bhushanvidhate109@gmail.com>
2026-06-15 11:05:04 +0530

    Add solution for Strictly Palindromic Number

---
+++ solutions/medium/Strictly Palindromic Number.cpp
@@ -0,0 +1,11 @@
+// Title: Strictly Palindromic Number
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/strictly-palindromic-number/
+
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+        return false;
+    }
+};
