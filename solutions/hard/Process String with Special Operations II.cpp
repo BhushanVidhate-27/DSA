ab160aa7e9f02585c5ca2b0a01c3ced2b954bf89
Bhushan Vidhate <bhushanvidhate109@gmail.com>
2026-06-17 23:11:16 +0530

    Add solution for Process String with Special Operations II

---
+++ solutions/hard/Process String with Special Operations II.cpp
@@ -0,0 +1,18 @@
+// Title: Process String with Special Operations II
+            // Difficulty: Hard
+            // Language: C++
+            // Link: https://leetcode.com/problems/process-string-with-special-operations-ii/
+
+
+        for (int i=n-1; i>=0; i--) {
+            ll L = len[i];
+            if (isalpha(s[i])) {
+                if(k == L-1)return s[i];
+        }
+            return '.';
+        if (curr == 0 || k >= curr) {
+        }
+            len[i] = curr;
+            } else if (s[i] == '#') {
+                if(L > 1) {
+                    k %= L/2;
