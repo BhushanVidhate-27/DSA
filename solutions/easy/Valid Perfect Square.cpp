7d81d13ca61ab78d3ca7f394fe7dcd0b6654dbf9
Bhushan Vidhate <bhushanvidhate109@gmail.com>
2026-06-24 11:12:48 +0530

    Add solution for Valid Perfect Square

---
+++ solutions/easy/Valid Perfect Square.cpp
@@ -0,0 +1,13 @@
+// Title: Valid Perfect Square
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/valid-perfect-square/
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int s = sqrt(num);
+    }
+        return s == num;
+        s *= s;
+};
