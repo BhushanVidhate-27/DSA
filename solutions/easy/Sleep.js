71eebe4bff01e5714c6883fc58d241fca699635d
Bhushan Vidhate <bhushanvidhate109@gmail.com>
2026-06-30 20:14:59 +0530

    Add solution for Sleep

---
+++ solutions/easy/Sleep.js
@@ -0,0 +1,19 @@
+// Title: Sleep
+            // Difficulty: Easy
+            // Language: JavaScript
+            // Link: https://leetcode.com/problems/sleep/
+
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+    await new Promise((res, rej)=>{
+}
+
+/** 
+ * let t = Date.now()
+        setTimeout(res, millis);
+    });
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
