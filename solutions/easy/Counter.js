b44033ead7883b5044d5d2a0c1fad857d50e3b12
Bhushan Vidhate <bhushanvidhate109@gmail.com>
2026-06-30 20:03:47 +0530

    Add solution for Counter

---
+++ solutions/easy/Counter.js
@@ -0,0 +1,22 @@
+// Title: Counter
+            // Difficulty: Easy
+            // Language: JavaScript
+            // Link: https://leetcode.com/problems/counter/
+
+ */
+var createCounter = function(n) {
+    
+    return function() {
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ * @return {Function} counter
+ * @param {number} n
+        return n-1;
+        n++;
+ */
