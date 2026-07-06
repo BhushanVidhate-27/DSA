d4d0cd74034b7254ffd81def57bd3e34aeaee1b7
Bhushan Vidhate <bhushanvidhate109@gmail.com>
2026-07-06 23:44:38 +0530

    Add solution for Remove Covered Intervals

---
+++ solutions/medium/Remove Covered Intervals.cpp
@@ -0,0 +1,22 @@
+// Title: Remove Covered Intervals
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/remove-covered-intervals/
+
+        ranges::sort(A, {}, [](auto& x) {
+            return pair{x[0], -x[1]};
+        });
+
+        int res = 0, r = 0;
+
+        for (auto& x : A) {
+            res += x[1] > r;
+            r = max(r, x[1]);
+        }
+
+        return res;
+    }
+};
+    int removeCoveredIntervals(vector<vector<int>>& A) {
+public:
+class Solution {
