dbee59f8a87a94a14929b9f2d696068b1fa337d4
Bhushan Vidhate <bhushanvidhate109@gmail.com>
2026-07-07 13:02:14 +0530

    Add solution for Next Permutation

---
+++ solutions/medium/Next Permutation.cpp
@@ -0,0 +1,21 @@
+// Title: Next Permutation
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/next-permutation/
+
+        }
+            i++;
+            j--;
+        while (i < j) {
+            mySwap(nums, i, j);
+        int i = start;
+        int j = nums.size() - 1;
+    void reverse(vector<int>& nums, int start) {
+    }
+
+        nums[j] = temp;
+        nums[i] = nums[j];
+        int temp = nums[i];
+    void mySwap(vector<int>& nums, int i, int j) {
+public:
+class Solution {
