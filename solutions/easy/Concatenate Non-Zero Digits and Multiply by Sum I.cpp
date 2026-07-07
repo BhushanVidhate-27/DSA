f366da2ddd0531ecd3dbd8a85ae249f58b124d64
Bhushan Vidhate <bhushanvidhate109@gmail.com>
2026-07-07 10:24:22 +0530

    Add solution for Concatenate Non-Zero Digits and Multiply by Sum I

---
+++ solutions/easy/Concatenate Non-Zero Digits and Multiply by Sum I.cpp
@@ -0,0 +1,21 @@
+// Title: Concatenate Non-Zero Digits and Multiply by Sum I
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/concatenate-non-zero-digits-and-multiply-by-sum-i/
+
+class Solution {
+public:
+    long long sumAndMultiply(int n) {
+        for(char i: s) {
+        }
+        string s = to_string(n);
+        string x = "0";
+            if(i != '0') {
+                x += i;
+            }
+    }
+            sum += i - '0';
+        long long sum = 0;
+        long long y = stoll(x);
+        return y * sum;
+};
