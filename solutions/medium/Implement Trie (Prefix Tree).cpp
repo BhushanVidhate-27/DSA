1826f14cc63b314cb412e08ed62278cabd59d2ae
Bhushan Vidhate <bhushanvidhate109@gmail.com>
2026-07-08 12:24:19 +0530

    Add solution for Implement Trie (Prefix Tree)

---
+++ solutions/medium/Implement Trie (Prefix Tree).cpp
@@ -0,0 +1,20 @@
+// Title: Implement Trie (Prefix Tree)
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/implement-trie-(prefix-tree)/
+
+    bool search(string word) {
+    
+    }
+        curr->end = true;
+        }
+            curr = curr->child[idx];
+            }
+                curr->child[idx] = new Node();
+            if(curr->child[idx] == NULL) {
+            int idx = ch - 'a';
+        for(char ch: word) {
+        Node* curr = root;
+    void insert(string word) {
+    
+    }
