161ae4839b7acb337e7bb9b30758617d3c7a0088
Bhushan Vidhate <bhushanvidhate109@gmail.com>
2026-07-19 08:17:34 +0530

    Add solution for Even Number of Knight Moves

---
+++ solutions/easy/Even Number of Knight Moves.cpp
@@ -0,0 +1,5 @@
+// Title: Even Number of Knight Moves
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/even-number-of-knight-moves/
+
