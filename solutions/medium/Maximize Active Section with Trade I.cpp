94da4c24d25bae9e99fa90962bb859d4d74269d4
Bhushan Vidhate <bhushanvidhate109@gmail.com>
2026-07-21 20:33:37 +0530

    Add solution for Maximize Active Section with Trade I

---
+++ solutions/medium/Maximize Active Section with Trade I.cpp
@@ -0,0 +1,6 @@
+// Title: Maximize Active Section with Trade I
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/maximize-active-section-with-trade-i/
+
+c
