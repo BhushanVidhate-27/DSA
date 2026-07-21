adb149697f90e217cd089b38e92e2bcbf0aed1f1
Bhushan Vidhate <bhushanvidhate109@gmail.com>
2026-07-21 21:12:11 +0530

    Add solution for Koko Eating Bananas

---
+++ solutions/medium/Koko Eating Bananas.cpp
@@ -0,0 +1,22 @@
+// Title: Koko Eating Bananas
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/koko-eating-bananas/
+
+    }
+        return ms <= 1LL*h;
+        }
+            }
+                ms +=(int) ceil((double)curr / m);
+            }else{
+            if(curr <= m) {
+                ms++;
+            int curr = nums[i];
+        for(int i=0; i<n; i++) {
+        ll ms = 0;//min hour required
+        int n = nums.size();
+    bool isValid(vector<int>& nums, int h, int m) {
+public:
+class Solution {
+    int minEatingSpeed(vector<int>& nums, int h) {
+        if(!nums.size())return 0;
