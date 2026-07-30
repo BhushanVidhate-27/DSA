ebe745f5b5430dd2200a54e14596e63bda49baf9
Bhushan Vidhate <bhushanvidhate109@gmail.com>
2026-07-30 12:50:18 +0530

    Add solution for Minimum Number of Pushes to Type Word I

---
+++ solutions/easy/Minimum Number of Pushes to Type Word I.cpp
@@ -0,0 +1,12 @@
+// Title: Minimum Number of Pushes to Type Word I
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-i/
+
+int LUT[27]={0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 14, 16, 18, 20, 22, 24, 27, 30, 33, 36, 39, 42, 45, 48, 52, 56};
+class Solution {
+public:
+    int minimumPushes(string word) {
+        return LUT[word.size()];
+    }
+};
