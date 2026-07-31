257b7b45d5e2f288b3e53de2320f74b743c422b9
Bhushan Vidhate <bhushanvidhate109@gmail.com>
2026-07-31 22:48:11 +0530

    Add solution for Minimum Number of Pushes to Type Word II

---
+++ solutions/medium/Minimum Number of Pushes to Type Word II.cpp
@@ -0,0 +1,23 @@
+// Title: Minimum Number of Pushes to Type Word II
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-ii/
+
+
+        sort(freq.begin(), freq.end());
+
+        int minPushing = 0;
+
+        for(int i = 25, pushCnt = 0; i >= 0; i--) {
+            int currEleIdx = 25 - i;
+
+            if(currEleIdx % 8 == 0)
+                pushCnt++;
+
+            freq[ch - 'a']++;
+        for(char ch : word)
+
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> freq(26, 0);
