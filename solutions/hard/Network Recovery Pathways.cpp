12e18e909d9e88eb070a9af5dd7d4fcc61985ac3
Bhushan Vidhate <bhushanvidhate109@gmail.com>
2026-07-31 22:51:19 +0530

    Add solution for Network Recovery Pathways

--- solutions/hard/Network Recovery Pathways.cpp
+++ solutions/hard/Network Recovery Pathways.cpp
@@ -3,21 +3,20 @@
             // Language: C++
             // Link: https://leetcode.com/problems/network-recovery-pathways/
 
-class Solution {
-public:
-    int n;
+        int res = -1;
 
-    bool find(int limit, unordered_map<int, vector<pair<int,int>>>& adj,
-              vector<bool>& online, vector<int>& topoSort, long long k){
+        while(low <= high){
+            int mid = low + (high - low)/2;
 
-        vector<long long> dist(n, LLONG_MAX);
-        dist[0] = 0;
+            if(find(mid, adj, online, topoSort, k)){
+                res = mid;
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
 
-        for(int node : topoSort){
-            if(dist[node] == LLONG_MAX) continue;
-
-            for(auto &nxt : adj[node]){
-                int nxtNode = nxt.first;
-                int cst = nxt.second;
-
-                if(cst < limit) continue;
+        return res;
+    }
+};
