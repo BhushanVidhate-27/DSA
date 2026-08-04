4085a3400a05c880512c1763587320fe7f316c0b
Bhushan Vidhate <bhushanvidhate109@gmail.com>
2026-08-04 20:13:25 +0530

    Add solution for String Compression

---
+++ solutions/medium/String Compression.cpp
@@ -0,0 +1,24 @@
+// Title: String Compression
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/string-compression/
+
+            }
+            if(cnt > 1) {
+                cnt++;j++;
+            while(j < n && chars[i] == chars[j]) {
+            int j = i+1;
+            int cnt = 1;
+        while(i < n) {
+        string s = "";
+        int i = 0;
+        int n = chars.size();
+    int compress(vector<char>& chars) {
+public:
+                s += chars[i];
+                s += to_string(cnt);
+            }else {
+                s += chars[i];
+            }
+            i = j;//updation
+        }
