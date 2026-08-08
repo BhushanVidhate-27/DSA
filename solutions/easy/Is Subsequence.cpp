84b2ecef98b41301f58f08945671e7f61fbb642c
Bhushan Vidhate <bhushanvidhate109@gmail.com>
2026-08-08 20:00:56 +0530

    Add solution for Is Subsequence

--- solutions/easy/Is Subsequence.cpp
+++ solutions/easy/Is Subsequence.cpp
@@ -3,16 +3,3 @@
             // Language: C++
             // Link: https://leetcode.com/problems/is-subsequence/
 
-class Solution {
-public:
-    bool isSubsequence(string s, string t) {
-        int i=0, j=0;
-        while(i<s.size() && j<t.size()) {
-            if(s[i] == t[j]) {
-                i++;
-            }
-        }
-    }
-            j++;
-        return i == s.size();
-};
