15418a2063ab473f5696ef2bf85629cc100a22fd
Bhushan Vidhate <bhushanvidhate109@gmail.com>
2026-08-09 12:25:09 +0530

    Add solution for Minimum Total Price After Applying Discounts

---
+++ solutions/medium/Minimum Total Price After Applying Discounts.cpp
@@ -0,0 +1,19 @@
+// Title: Minimum Total Price After Applying Discounts
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/minimum-total-price-after-applying-discounts/
+
+        ld ans = 0;
+        ll j=0;
+        ll i=0;
+        ll m = discounts.size();
+        ll n = prices.size();
+
+        sort(discounts.begin(), discounts.end(), greater<>());
+        sort(prices.begin(), prices.end(), greater<>());
+    double minPrice(vector<int>& prices, vector<int>& discounts) {
+public:
+class Solution {
+using namespace std;
+using ll = long long;
+using ld = long double;
