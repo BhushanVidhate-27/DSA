2d823e21535c545cc76e27a05d7e0610cc0f5dac
Bhushan Vidhate <bhushanvidhate109@gmail.com>
2026-08-13 22:11:50 +0530

    Add solution for Weighted Sum of a Tree

--- solutions/medium/Weighted Sum of a Tree.cpp
+++ solutions/medium/Weighted Sum of a Tree.cpp
@@ -3,6 +3,8 @@
             // Language: C++
             // Link: https://leetcode.com/problems/weighted-sum-of-a-tree/
 
+        vector<vector<int>> adj(n, vector<int>(0));
+
         for (int i = 0; i < n; i++) {
             if (parent[i] != -1) {
                 adj[parent[i]].push_back(i);
@@ -13,3 +15,11 @@
             if (parent[i] == -1) {
                 root = i;
                 break;
+            }
+        }
+        ll h = (ll)dfsHeight(adj, root, 0);
+        ll ans = dfsCalc(adj, nums, 0, h+1, 1);
+        cout << h << "..." << ans << endl;
+        return ans;
+    }
+};
