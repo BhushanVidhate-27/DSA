5ad8b77f6a7dc7ef036695e9e815aace2c84456e
Bhushan Vidhate <bhushanvidhate109@gmail.com>
2026-08-15 21:13:44 +0530

    Add solution for K-th Digit in Infinite String

---
+++ solutions/medium/K-th Digit in Infinite String.cpp
@@ -0,0 +1,5 @@
+// Title: K-th Digit in Infinite String
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/k-th-digit-in-infinite-string/
+
