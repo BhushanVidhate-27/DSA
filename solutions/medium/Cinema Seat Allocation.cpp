63497ea6e6d808269bf2db1045a767392a54f1cb
Bhushan Vidhate <bhushanvidhate109@gmail.com>
2026-08-19 22:07:46 +0530

    Add solution for Cinema Seat Allocation

---
+++ solutions/medium/Cinema Seat Allocation.cpp
@@ -0,0 +1,29 @@
+// Title: Cinema Seat Allocation
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/cinema-seat-allocation/
+
+        vector<bool> reserved(11, false);
+
+        int l = 0, r = 0;
+        int prev = 0;
+
+        while (r < m) {
+        int m = reservedSeats.size();
+        int ans = 0;
+
+
+        sort(reservedSeats.begin(), reservedSeats.end());
+
+    }
+
+    int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
+            }
+        }
+        return true;
+        for (int i = l; i <= r; i++) {
+            if (reserved[i]) {
+                return false;
+class Solution {
+public:
+    bool check(vector<bool>& reserved, int l, int r) {
