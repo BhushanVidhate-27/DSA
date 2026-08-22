9d5de54714c8ed15627903b6ae2350c9401b2012
Bhushan Vidhate <bhushanvidhate109@gmail.com>
2026-08-22 20:35:24 +0530

    Add solution for Distribute Elements Into Two Arrays I

---
+++ solutions/easy/Distribute Elements Into Two Arrays I.cpp
@@ -0,0 +1,23 @@
+// Title: Distribute Elements Into Two Arrays I
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/distribute-elements-into-two-arrays-i/
+
+        }
+            }else {
+                b.push_back(nums[i]);
+            }
+        for(auto i: a){
+        vector<int>ans;
+                a.push_back(nums[i]);
+            ans.push_back(i);
+            if(a.back() > b.back()) {
+        for(int i=2; i<n; i++) {
+        }
+
+        for(auto i: b){
+            ans.push_back(i);
+        }
+        return ans;
+    }
+};
